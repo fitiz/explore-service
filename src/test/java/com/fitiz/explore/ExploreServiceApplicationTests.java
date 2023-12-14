695746e91f86509d1ee6b5175cc301bee402e181
burakemreozer <burak.oezer@checktur.io>
2023-12-14 04:18:48 +0100

    init repo

---
+++ src/test/java/com/fitiz/explore/ExploreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitiz.explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
