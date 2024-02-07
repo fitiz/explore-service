ea38f1e8c4ae77308da88b8658f257cfc3856240
Evtim Kostadinov <evtim.kostadinov2000@gmail.com>
2024-02-07 02:01:43 +0100

    enabled eureka client

--- src/main/java/com/fitiz/explore/controller/HelloWorldController.java
+++ src/main/java/com/fitiz/explore/controller/HelloWorldController.java
@@ -2,11 +2,19 @@ package com.fitiz.explore.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/explore")
 public class HelloWorldController {
-    @GetMapping(value = "/")
+    @GetMapping("/public/hello")
     public String hello() {
         return "Hello world!";
     }
+
+    @GetMapping("/private/hello")
+    public String privateHello() {
+        return "Hello private world!";
+    }
+
 }
